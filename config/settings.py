777b84bb852b1450e976dfafe4076b7ad13eb37f
sa77irak <sa77irak@gmail.com>
2019-06-10 12:27:23 +0900

    changed debug mode to false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*g-_ddpn@w$7k#(&)%13ct!8!rsv4hm*k8cjsy0+@_h&-fq!a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
